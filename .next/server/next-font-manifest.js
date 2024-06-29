3c4a0b886bc87f59306a936df7628235e3e77362
PIYUSH KUMAR <93852522+piyushkumar24@users.noreply.github.com>
2024-06-30 03:10:43 +0530

    Delete .next directory

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/piyushkumar/Desktop/budget_tracker-main/app/layout\":[\"static/media/c9a5bc6a7c948fb0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
